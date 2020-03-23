6aff6a343510b52d3e628261307d330884dfc8d8
Vlad Artemyev <mrlzlamogilev@gmail.com>
2020-03-23 17:49:57 +0300

    Fix

--- config.py
+++ config.py
@@ -8,18 +8,18 @@ configurations = {
         DATA_ROOT = './data', # the parent root where your train/val/test data are stored
         MODEL_ROOT = './model', # the root to buffer your checkpoints
         LOG_ROOT = './log', # the root to log your train/val status
-        BACKBONE_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
-        HEAD_RESUME_ROOT = './', # the root to resume training from a saved checkpoint
+        BACKBONE_RESUME_ROOT = None,#'model/Backbone_GhostNet_Epoch_9_Batch_118710_Time_2020-03-05-13-07_checkpoint.pth',#'model/Backbone_ShuffleNetV2_2.0_Epoch_19_Batch_250610_Time_2020-03-02-11-42_checkpoint.pth',
+        HEAD_RESUME_ROOT = None,#'model/Head_ArcFace_Epoch_9_Batch_118710_Time_2020-03-05-13-07_checkpoint.pth',#'model/Head_ArcFace_Epoch_19_Batch_250610_Time_2020-03-02-11-42_checkpoint.pth',
 
-        BACKBONE_NAME = 'ShuffleNetV2_1.0', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
+        BACKBONE_NAME = 'MobileFaceNet', # support: ['ResNet_50', 'ResNet_101', 'ResNet_152', 'IR_50', 'IR_101', 'IR_152', 'IR_SE_50', 'IR_SE_101', 'IR_SE_152']
         HEAD_NAME = 'ArcFace', # support:  ['Softmax', 'ArcFace', 'CosFace', 'SphereFace', 'Am_softmax']
         LOSS_NAME = 'Focal', # support: ['Focal', 'Softmax']
 
         INPUT_SIZE = [112, 112], # support: [112, 112] and [224, 224]
         RGB_MEAN = [0.5, 0.5, 0.5], # for normalize inputs to [-1, 1]
         RGB_STD = [0.5, 0.5, 0.5],
-        EMBEDDING_SIZE = 1024, # feature dimension
-        BATCH_SIZE = 56,
+        EMBEDDING_SIZE = 512, # feature dimension
+        BATCH_SIZE = 128,
         DROP_LAST = True, # whether drop the last batch to ensure consistent batch_norm statistics
         LR = 0.01, # initial LR
         NUM_EPOCH = 125, # total epoch number (use the firt 1/25 epochs to warm up)
