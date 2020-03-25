a215c30fde37f8368f84e9cd3503615b6fff07f3
vladislav.artemyev <vladislav.artemyev@targetai.kz>
2020-03-25 17:49:54 +0600

    datasets

---
+++ datasets.sh
@@ -0,0 +1,7 @@
+gdown https://drive.google.com/uc?id=1WO5Meh_yAau00Gm2Rz2Pc0SRldLQYigT
+gdown https://drive.google.com/uc?id=1kpmcDeDmPqUcI5uX0MCBzpP_8oQVojzW
+gdown https://drive.google.com/uc?id=14vPvDngGzsc94pQ4nRNfuBTxdv7YVn2Q
+gdown https://drive.google.com/uc?id=1N7QEEQZPJ2s5Hs34urjseFwIoPVSmn4r
+gdown https://drive.google.com/uc?id=1AoZrZfym5ZhdTyKSxD0qxa7Xrp2Q1ftp
+gdown https://drive.google.com/uc?id=1-sDn79lTegXRNhFuRnIRsgdU88cBfW6V
+gdown https://drive.google.com/uc?id=1Lqvh24913uquWxa3YS_APluEmbNKQ4Us
